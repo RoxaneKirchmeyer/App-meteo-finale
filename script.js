5c1074a36b5de03e61c9fb22f72068e8fa5a4043
RoxaneKirchmeyer <roxane.kirchmeyer@gmail.com>
2023-12-05 16:11:38 +0100

    Finalisation

--- script.js
+++ script.js
@@ -1,30 +1,12 @@
-
-// display block et none sur la carte
-// une seule carte de description qui se modifie en fonction de la ville choisie
-
-// méthode simple
-// btnParis = document.querySelector("#paris");
-// btnLille = document.querySelector("#lille");
-// btnBordeaux = document.querySelector("#bordeaux");
-// btnStrasbourg = document.querySelector("#strasbourg");
-// btnMontpellier = document.querySelector("#montpellier");
-// btnNantes = document.querySelector("#nantes");
-// btnNice = document.querySelector("#nice");
-// btnToulouse = document.querySelector("#toulouse");
-// btnLyon = document.querySelector("#lyon");
-// btnMarseille = document.querySelector("#marseille");
-
-// Au clic d'un button ayant l'id de la ville, un evenement au clic qui va fetch et affiche le climat de la ville
-
 // Méthode optimisé
 
-// faire un append child de la bulle de dialogue
 const buttonsVilles = document.querySelectorAll('button')
 const divContenu = document.createElement('div')
 divContenu.className = "contenu"
-document.body.appendChild(divContenu)
 const infoMeteo = document.createElement('p')
 divContenu.appendChild(infoMeteo)
+const miss = document.querySelector(".miss")
+miss.appendChild(divContenu)
 
 
 // Pour chaque buttons, au clic, affiche la météo
@@ -35,46 +17,32 @@ buttonsVilles.forEach(ville => {
             .then(data => {
                 const type = ville.id
                 console.log(type)
-             
+
 
                 let backgroundImageURL = ''
-    const villeSelectionnee = data.find(ville => ville.nom === type);
-    console.log(villeSelectionnee)
-    // Vérifier si la ville sélectionnée a des conditions météorologiques pluvieuses
-    if (villeSelectionnee.conditionsMeteo.includes('Pluvieux')) {
-        // Définir l'image de fond pour les conditions météorologiques pluvieuses
-        backgroundImageURL = 'img/pluie-opacity.png';
-    } else {
-        // Définir l'image de fond pour d'autres conditions météorologiques
-        backgroundImageURL = 'img/pexels-skitterphoto-3768.jpg';
-    }
-    document.body.style.backgroundImage = `url('${backgroundImageURL}')`;
-    if (villeSelectionnee) {
-        // Afficher les détails météo pour la ville sélectionnée
-        infoMeteo.textContent = `Bonjour à tous, bienvenue pour votre bulletin
-         météo. Aujourd'hui à ${villeSelectionnee.nom}, nous 
-         observons un ciel ${villeSelectionnee.conditionsMeteo} avec une 
-         température actuelle de ${villeSelectionnee.temperature} degrés Celsius. 
+                const villeSelectionnee = data.find(ville => ville.nom === type);
+                // Vérifier si la ville sélectionnée a des conditions météorologiques pluvieuses
+                if (villeSelectionnee.conditionsMeteo.includes('Pluvieux')) {
+                    // Définir l'image de fond pour les conditions météorologiques pluvieuses
+                    backgroundImageURL = 'img/pluie-opacity.png';
+                } else {
+                    // Définir l'image de fond pour d'autres conditions météorologiques
+                    backgroundImageURL = 'img/pexels-skitterphoto-3768.jpg';
+                }
+                document.body.style.backgroundImage = `url('${backgroundImageURL}')`;
+                if (villeSelectionnee) {
+                    // Afficher les détails météo pour la ville sélectionnée
+                    infoMeteo.textContent = `Bonjour à tous, bienvenue pour votre bulletin
+         météo. Aujourd'hui à ${villeSelectionnee.nom.toUpperCase()}, nous 
+         observons un ciel ${villeSelectionnee.conditionsMeteo.toLowerCase()} avec une 
+         température actuelle de ${villeSelectionnee.temperature} degrés. 
          L'humidité est de ${villeSelectionnee.humidite} % et nous prévoyons des précipitations de l'ordre de ${villeSelectionnee.precipitations} mm. 
          L'indice UV sera de ${villeSelectionnee.indiceUV}`;
-    }
+                }
             }
             )
     })
 
 })
 
-// selectionner All bouton {
-
-    // buttonsVilles.forEach(villes => {
-
-    // ajouter un evenement au bouton qui ouvre la div d'infos
-
-        // creer une div d'infos de la ville correspondante avec un if (id button = nom du fetch)
-            // dans cette divInfoMeteos mettre les infos de la ville correspondante avec le texte evelyne
-            // changer les valeurs des données météos en fonction de la ville
-
-        // si on reclique : supprime la div
-
-//})}
 
